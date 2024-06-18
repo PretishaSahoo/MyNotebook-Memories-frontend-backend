c6853f1bfe709031a26394f0992f61ba6340dfc3
Pretisha Sahoo <115354635+PretishaSahoo@users.noreply.github.com>
2024-06-18 14:17:26 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,18 @@
 const express = require('express');
 const connectToMongo = require("./db");
 const cors = require('cors');
+const dotenv = require("dotenv");
 
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+dotenv.config();
 
 // Connect to MongoDB
 connectToMongo()
